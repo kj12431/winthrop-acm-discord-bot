fc76847bda3e25b66776bb3bcfdc19f8cb4b4043
Kevin Justin <kevin.justin04@gmx.com>
2023-10-11 16:30:02 -0400

    working, simple bot

--- bot.py
+++ bot.py
@@ -1,12 +1,27 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+import os 
+
+from dotenv import load_dotenv
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send('hi there')
+
 # Logic goes here
 
-bot.run('')
+bot.run(DISCORD_TOKEN)
